e5a2fbd186c29921bbf9893814576e20e2391c18
macaronij <macaronij@gmail.com>
2023-01-15 12:14:43 -0300

    agregando editar about

--- src/main/java/basededatos/portfolio3/Controller/EditandoController.java
+++ src/main/java/basededatos/portfolio3/Controller/EditandoController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import basededatos.portfolio3.Model.About;
 import basededatos.portfolio3.Model.Education;
 import basededatos.portfolio3.Model.Proyect;
+import basededatos.portfolio3.Repository.AboutRepository;
 import basededatos.portfolio3.Repository.EducationRepository;
 import basededatos.portfolio3.Repository.ProyectRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -21,10 +23,12 @@ public class EditandoController {
 
     EducationRepository educationRepo;
     ProyectRepository proyectRepo;
+    AboutRepository aboutRepo;
 
-    public EditandoController(EducationRepository _educactionRepo, ProyectRepository _proyectRepo) {
+    public EditandoController(EducationRepository _educactionRepo, ProyectRepository _proyectRepo, AboutRepository _aboutRepo) {
         educationRepo = _educactionRepo;
         proyectRepo = _proyectRepo;
+        aboutRepo = _aboutRepo;
     }
 
     @PostMapping("/education/{idLinea}")
@@ -49,8 +53,8 @@ public class EditandoController {
 
     @PostMapping("/proyect/{idLinea}")
     public Boolean editarProyecto(@RequestBody Proyect datos, @PathVariable String idLinea){
-        log.info("Editando EDUCACION");
-        log.info("El ide de linea es: "+idLinea);
+        log.info("Editando PROYECT");
+        log.info("El id de linea es: "+idLinea);
         Long id = Long.parseLong(idLinea);
         if (proyectRepo.findById(id).isPresent()) {
             log.info("encontre el ID en proyect");
@@ -61,6 +65,24 @@ public class EditandoController {
             pro.setEnlace(datos.getEnlace());
             pro.setFecha(datos.getFecha());
             proyectRepo.save(pro);
+            return true;
+        } else {
+            log.info("Error no se encontro el ID");
+            return false;
+        }
+    }
+
+    @PostMapping("/about/{idLinea}")
+    public Boolean editarAbout(@RequestBody About datos, @PathVariable String idLinea){
+        log.info("Editando ABOUT");
+        log.info("El id de linea es: "+idLinea);
+        Long id = Long.parseLong(idLinea);
+        if (aboutRepo.findById(id).isPresent()) {
+            log.info("encontre el ID en about");
+            About sobremi = aboutRepo.findById(id).get();
+            sobremi.setTitulo(datos.getTitulo());
+            sobremi.setTexto(datos.getTexto());
+            aboutRepo.save(sobremi);
             return true;
         } else {
             log.info("Error no se encontro el ID");
